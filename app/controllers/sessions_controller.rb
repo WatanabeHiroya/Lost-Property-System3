a4d747e295717ce982807496e33ce6cf73424774
watanabe hiroya <plastic.gu@gmail.com>
2020-05-06 09:18:54 +0000

    ログイン機能実装中

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #ログイン
+      flash[:success] = "ログインしました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "認証に失敗しました。"
+      render :new
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   
   resources :users
 end
