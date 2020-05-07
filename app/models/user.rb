db9fe1044be46f74d5c343118ddf9de93ac1178c
watanabe hiroya <plastic.gu@gmail.com>
2020-05-07 12:00:08 +0000

    ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,4 +10,37 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
+  # 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # トークンがダイジェストと一致すればtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ログイン情報を破棄
+  def forget
+    return false if remember_digest.nil? # ダイジェストが存在しない場合はfalseを返して終了
+    update_attribute(:remember_digest, nil)
+  end
+ 
 end
