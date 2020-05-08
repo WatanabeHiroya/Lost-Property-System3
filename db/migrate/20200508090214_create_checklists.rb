e3c21fab3d3d7e54f31397b8bf5e4acdfb7e1ccc
watanabe hiroya <plastic.gu@gmail.com>
2020-05-08 09:27:29 +0000

    checklistモデル、コントローラ追加

---
+++ db/migrate/20200508090214_create_checklists.rb
@@ -0,0 +1,13 @@
+class CreateChecklists < ActiveRecord::Migration[5.1]
+  def change
+    create_table :checklists do |t|
+      t.string :object
+      t.datetime :departure_time
+      t.text :note
+      t.string :check
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505120321) do
+ActiveRecord::Schema.define(version: 20200508090214) do
+
+  create_table "checklists", force: :cascade do |t|
+    t.string "object"
+    t.datetime "departure_time"
+    t.text "note"
+    t.string "check"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_checklists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
