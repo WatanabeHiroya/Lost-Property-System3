9fa3164b03a15bb40e4049bfebfb7ec9f6cfb330
watanabe hiroya <plastic.gu@gmail.com>
2020-05-09 08:00:43 +0000

    Finish japanization-and-time-format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   end
   
   def update
-    if @user.updateattributes(user_params)
+    if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
