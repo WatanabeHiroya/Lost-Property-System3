83fa6a683b82a8b10bb2391fc5b346daaf7c4886
watanabe hiroya <plastic.gu@gmail.com>
2020-05-29 12:15:28 +0000

    メール送信成功

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: '忘れ物アプリ'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,15 +52,15 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    port:                 587,
     address:              'smtp.gmail.com',
-    port:                  587,
-    domain:               'gmail.com',
-    user_name:            '<lost.property.system@gmail.com>',
-    password:             '<hiro9138>',
-    authentication:       'plain',
-    enable_starttls_auto:  true
+    domain:               'smtp.gmail.com',
+    user_name:            'lost.property.system@gmail.com',
+    password:             'gacikivnerfluetp',
+    authentication:       'login',
+    enable_starttls_auto: true
   }
 end
