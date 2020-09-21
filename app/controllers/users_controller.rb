54f05a6d73d6c3d59f513eee87b13800b55d59b8
watanabe hiroya <plastic.gu@gmail.com>
2020-09-21 08:07:54 +0000

    管理者権限変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
-  before_action :admin_or_correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :admin_or_correct_user, only: [:edit, :update, :destroy]
   before_action :admin_user, only: [:index]
   before_action :before_new, only: [:new]
+  before_action :correct_user, only: [:show]
 
   def index
     @users = User.paginate(page: params[:page])
