3ed69f2d90a4a5f104e4d02a191993c8b0016b2b
watanabehiroya <plastic.gu@gmail.com>
2020-10-11 04:48:49 +0000

    production.rbメール設定記載

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # メール通知
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            'lost.property.system@gmail.com',
+    password:             'gacikivnerfluetp',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+  
 end
